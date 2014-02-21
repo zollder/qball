a38b1d77382b287b51b496bc41254052c2901d2c
Eugene Simon <eugen@publicbikesystem.com>
2014-02-21 00:19:16 -0500

    added StreamServer implementation to simulate QBall device operation

---
+++ StreamServer.h
@@ -0,0 +1,65 @@
+#include <cstdlib>
+#include <iostream.h>
+#include <pthread.h>
+
+#include "CSocket.h"
+#include "StreamWriter.h"
+#include "PulseTimer.h"
+
+#ifndef streamserver_h
+#define streamserver_h
+
+//-----------------------------------------------------------------------------------------
+// StreamServer interface.
+//-----------------------------------------------------------------------------------------
+class StreamServer
+{
+	//-----------------------------------------------------------------------------------------
+	// Public members
+	//-----------------------------------------------------------------------------------------
+	public:
+
+		// constructor
+		StreamServer(unsigned short int port, char address, double interval);
+
+		// destructor
+		~StreamServer();
+
+		// sets server port
+		void setPort(unsigned short int port);
+
+		// sets server IP address
+		void setAddress(char address);
+
+		// sets pulse timer interval
+		void setTimeInterval(double interval);
+
+		// returns server port
+		unsigned short int getPort();
+
+		// returns server port
+		char* getAddress();
+
+		// returns pulse timer interval
+		char getTimeInterval();
+
+		// enables and starts the client
+		void start();
+
+		// stops and cleans the client
+		void stop();
+
+	//-----------------------------------------------------------------------------------------
+	// Private members
+	//-----------------------------------------------------------------------------------------
+    private:
+		unsigned short int serverPort;
+		char serverAddress;
+		double timeInterval;
+
+		CSocket* serverSocket;
+		StreamWriter* streamWriter;
+		PulseTimer* streamWriterTimer;
+};
+
+#endif
