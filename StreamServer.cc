26f0f391a0b68798faad7eecf6e1451cb0926bc4
Eugene Simon <eugen@publicbikesystem.com>
2014-02-21 01:29:11 -0500

    updated StreamServer implementation

--- StreamServer.cc
+++ StreamServer.cc
@@ -3,48 +3,46 @@
 #include <iostream.h>
 #include <pthread.h>
 
-#include "StreamClient.h"
-#include "StreamClient.h"
+#include "StreamServer.h"
+#include "StreamServer.h"
 
 //---------------------------------------------------------------------------------------------
-// StreamClient subclass implementation.
+// StreamServer subclass implementation.
 //---------------------------------------------------------------------------------------------
 
 	//-----------------------------------------------------------------------------------------
 	// Constructor
 	//-----------------------------------------------------------------------------------------
-	StreamClient::StreamClient(unsigned short int hostPort, char hostAddress, double timeInterval)
+	StreamServer::StreamServer(unsigned short int hostPort, char hostAddress, double timeInterval)
 	{
-		printf("Constructing StreamClient ...\n");
+		printf("Constructing StreamServer ...\n");
 
 		setPort(hostPort);
 		setAddress(hostAddress);
 		setTimeInterval(timeInterval);
 
 		// instantiate objects
-		clientSocket = new CSocket();
-		qballData = new QballData();
-		streamReader = new StreamReader(mutex, qballData, clientSocket);
-		streamReaderTimer = new PulseTimer(getTimeInterval(), streamReader->getChannelId());
+		serverSocket = new CSocket();
+		streamWriter = new StreamWriter(mutex, serverSocket);
+		streamWriterTimer = new PulseTimer(getTimeInterval(), streamWriter->getChannelId());
 	}
 
 	/**-----------------------------------------------------------------------------------------
 	 * Destructor
 	-----------------------------------------------------------------------------------------*/
-	StreamClient::~StreamClient()
+	StreamServer::~StreamServer()
 	{
-		printf("Destroying StreamClient ...\n");
+		printf("Destroying StreamServer ...\n");
 
-		delete clientSocket;
-		delete qballData;
-		delete streamReader;
-		delete streamReaderTimer;
+		delete serverSocket;
+		delete streamWriter;
+		delete streamWriterTimer;
 	}
 
 	/**-----------------------------------------------------------------------------------------
 	 * Sets server port.
 	-----------------------------------------------------------------------------------------*/
-	void StreamClient::setPort(unsigned short int port)
+	void StreamServer::setPort(unsigned short int port)
 	{
 		serverPort = port;
 	}
@@ -52,7 +50,7 @@
 	/**-----------------------------------------------------------------------------------------
 	 * Sets server IP address.
 	-----------------------------------------------------------------------------------------*/
-	void StreamClient::setAddress(char address)
+	void StreamServer::setAddress(char address)
 	{
 		serverAddress = address;
 	}
@@ -60,7 +58,7 @@
 	/**-----------------------------------------------------------------------------------------
 	 * Sets pulse timer interval.
 	-----------------------------------------------------------------------------------------*/
-	void StreamClient::setTimeInterval(double interval)
+	void StreamServer::setTimeInterval(double interval)
 	{
 		timeInterval = interval;
 	}
@@ -68,7 +66,7 @@
 	/**-----------------------------------------------------------------------------------------
 	 * Returns server port.
 	-----------------------------------------------------------------------------------------*/
-	unsigned short int StreamClient::getPort()
+	unsigned short int StreamServer::getPort()
 	{
 		return serverPort;
 	}
@@ -76,7 +74,7 @@
 	/**-----------------------------------------------------------------------------------------
 	 * Returns server IP address.
 	-----------------------------------------------------------------------------------------*/
-	char* StreamClient::getAddress()
+	char* StreamServer::getAddress()
 	{
 		return &serverAddress;
 	}
@@ -84,33 +82,28 @@
 	/**-----------------------------------------------------------------------------------------
 	 * Returns pulse timer interval.
 	-----------------------------------------------------------------------------------------*/
-	char StreamClient::getTimeInterval()
+	char StreamServer::getTimeInterval()
 	{
 		return timeInterval;
 	}
 
 	/**-----------------------------------------------------------------------------------------
-	 * Prepares and starts the client:
-	 * - connects to remote server;
-	 * - starts StreamReader thread;
-	 * - enables pulse timer;
+	 * Prepares and starts the server:
 	-----------------------------------------------------------------------------------------*/
-	void StreamClient::start()
+	void StreamServer::start()
 	{
-		clientSocket->connectSocket(getPort(), getAddress());
-		streamReader->start();
-		streamReaderTimer->start();
+		serverSocket->bindName(getPort());
+		streamWriter->start();
+		streamWriterTimer->start();
 	}
 
 	/**-----------------------------------------------------------------------------------------
-	 * Stops and cleans up the client:
-	 * - closes the socket session;
-	 * - stops (joins) StreamReader thread;
-	 * - stops pulse timer;
+	 * Stops and cleans up the server:
 	-----------------------------------------------------------------------------------------*/
-	void StreamClient::stop()
+	void StreamServer::stop()
 	{
-		clientSocket->closeSession();
-		streamReader->join();
-		streamReaderTimer->stop();
+		serverSocket->closeSession();
+		serverSocket->closeSocket();
+		streamWriter->join();
+		streamWriterTimer->stop();
 	}
